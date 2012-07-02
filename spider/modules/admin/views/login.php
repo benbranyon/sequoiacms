4df322c33b72172b4069030a50c49f343153e3c2
Ben Branyon <Ben.B@quantumrewards.com>
2012-07-02 15:22:26 -0700

    fix admin.css for login form

--- spider/modules/admin/views/login.php
+++ spider/modules/admin/views/login.php
@@ -1,7 +1,7 @@
 <div id="admin-login">
 	<?php echo '<form method="POST" enctype="multipart/form-data" action="/' . BASE_DIR . '/admin/login" id = "login">';?>
 	<form method="post" action="/admin/login" id="login">
-		<h1>Log In</h1>
+		<h1  class="admin-login">Log In</h1>
 		<fieldset id="inputs">
 			<input id="username" name="username" type="text" placeholer="Username" autofocus required>
 			<input id="password" name="password" type="password" placeholer="Password" required>
